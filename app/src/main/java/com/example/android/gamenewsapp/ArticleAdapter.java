f7739110c72b29e1e1c18de66417397d349737e8
egrunwald <egrunwald112@gmail.com>
2021-04-25 23:38:42 -0700

    feat: Added new method formatDate.
    This method takes the relevant date data from the date string
    and reformats it for display.

--- app/src/main/java/com/example/android/gamenewsapp/ArticleAdapter.java
+++ app/src/main/java/com/example/android/gamenewsapp/ArticleAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.gamenewsapp;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +11,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ArticleAdapter extends ArrayAdapter<Article> {
+    public static final String LOG_TAG = ArrayAdapter.class.getName();
 
     public ArticleAdapter(Activity context, List<Article>articles) {
         super(context, 0, articles);
@@ -32,7 +38,7 @@ public class ArticleAdapter extends ArrayAdapter<Article> {
         String articleTitle = currentArticle.getArticleData("title");
         String articleAuthor = currentArticle.getArticleData("author");
         String articleSection = currentArticle.getArticleData("section");
-        String articleDate = currentArticle.getArticleData("date");
+        String articleDate = formatDate(currentArticle.getArticleData("date"));
 
         TextView titleTextView = listItemView.findViewById(R.id.tv_title);
         titleTextView.setText(articleTitle);
@@ -47,5 +53,19 @@ public class ArticleAdapter extends ArrayAdapter<Article> {
         dateTextView.setText(articleDate);
 
         return listItemView;
+    }
+
+    private String formatDate(String jsonDate) {
+        SimpleDateFormat jsonDateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "LLL dd, yyyy", Locale.US);
+        try {
+            Date jsonDateParsed = jsonDateFormat.parse(jsonDate);
+            return dateFormat.format(jsonDateParsed);
+        } catch (ParseException e) {
+            Log.e(LOG_TAG, "problem formatting date.", e);
+            return "Bad Date Format";
+        }
     }
 }
